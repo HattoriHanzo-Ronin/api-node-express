7e1fa494b2aee32874304b0d921ac474746a6582
Hattori <frannsm97@gmail.com>
2026-05-17 12:21:56 +0100

    refactor: clean route formatting and naming consistency

---
+++ routes/ftp-router.js
@@ -0,0 +1,24 @@
+import { Router } from "express";
+import { FtpController } from "../controllers/ftp/ftp-controller.js";
+import Middlewares from "../middlewares/middlewares.js";
+
+export default function createFtpRouter({ ftpModel }) {
+    const router = Router();
+    const ftpController = new FtpController({ ftpModel });
+
+    router.get("/", ftpController.dir);
+
+    router.get("/download", ftpController.getFile);
+
+    router.get("/download/:id", ftpController.getFile);
+
+    router.post("/mkdir", ftpController.makeDir);
+
+    router.post("/upload", Middlewares.mult().single("file"), ftpController.upload);
+
+    router.post("/download", ftpController.download);
+
+    router.delete("/:type", ftpController.delete);
+
+    return router;
+}
