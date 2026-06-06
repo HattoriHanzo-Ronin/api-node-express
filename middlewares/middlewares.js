c94e1dde88f4b2019e54e1b581dc59cfc7ee7c29
Hattori <frannsm97@gmail.com>
2026-06-06 17:38:44 +0100

    feat: implement centralized error handling

--- middlewares/middlewares.js
+++ middlewares/middlewares.js
@@ -1,9 +1,10 @@
 import express from "express";
 import cors from "cors";
 import multer from "multer";
+import ApiError from "../utils/api-error.js";
 
 /**
- * Application middleware factory.
+ * Application middleware factory
  *
  * @author HattoriHanzo-Ronin
  */
@@ -29,12 +30,26 @@ export default class Middlewares {
     }
 
     /**
-     * Wraps an async Express handler and forwards errors to the next middleware.
+     * Wraps an async Express handler and forwards errors to the next middleware
      *
      * @param {Function} fn Async Express route handler
      * @returns {Function} Express middleware
      */
     static asyncHandler(fn) {
         return (req, res, next) => Promise.resolve(fn(req, res, next)).catch(next);
+    }
+
+    /**
+     * Handles application errors
+     */
+    static errorHandler(err, req, res, next) {
+        const status = err.status || 500;
+        const message = err instanceof ApiError ? err.message : "Error inesperado";
+        let result = { message };
+        if (err.details) {
+            result = { ...result, details: err.details };
+        }
+        
+        res.status(status).json(result);
     }
 }
