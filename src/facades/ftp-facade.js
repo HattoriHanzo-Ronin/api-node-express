b6983ee69146fa40d3315e9cf115d5b85fabad6f
Hattori <frannsm97@gmail.com>
2026-08-15 18:19:18 +0000

    feat: add versioning to FTP operations

---
+++ src/facades/ftp-facade.js
@@ -0,0 +1,97 @@
+/**
+ * FTP facade
+ *
+ * @author HattoriHanzo-Ronin
+ */
+export default class FtpFacade {
+    constructor({ ftpService, dataVersionsService, tx }) {
+        this.ftpService = ftpService;
+        this.dataVersionsService = dataVersionsService;
+        this.tx = tx;
+    }
+
+    /**
+     * Returns FTP directory content
+     *
+     * @param {Object} data FTP directory data
+     * @returns {Promise<{ version: string, data: Object[] }>} Directory resources
+     */
+    async dir(data) {
+        const [{ version }, result] = await Promise.all([
+            this.dataVersionsService.getById({ id: "ftp" }),
+            this.ftpService.dir(data)
+        ]);
+        return { version, data: result };
+    }
+
+    /**
+     * Creates a FTP directory and increments its data version
+     *
+     * @param {Object} data FTP directory data
+     * @returns {Promise<Object>} Created directory
+     */
+    async makeDir(data) {
+        return this.#mutate(() => this.ftpService.makeDir(data));
+    }
+
+    /**
+     * Moves FTP resources and increments their data version
+     *
+     * @param {Object} data FTP move data
+     * @returns {Promise<Object>} Moved resources
+     */
+    async move(data) {
+        return this.#mutate(() => this.ftpService.move(data));
+    }
+
+    /**
+     * Renames a FTP resource and increments its data version
+     *
+     * @param {Object} data FTP rename data
+     * @returns {Promise<Object>} Renamed resource
+     */
+    async rename(data) {
+        return this.#mutate(() => this.ftpService.rename(data));
+    }
+
+    /**
+     * Uploads FTP resources and increments their data version
+     *
+     * @param {Object} data FTP upload data
+     * @returns {Promise<Object[]>} Uploaded resources
+     */
+    async upload(data) {
+        return this.#mutate(() => this.ftpService.upload(data));
+    }
+
+    /**
+     * Downloads FTP resources
+     *
+     * @param {Object} data FTP download data
+     * @returns {Promise<string>} Downloaded resource path
+     */
+    async download(data) {
+        return this.ftpService.download(data);
+    }
+
+    /**
+     * Deletes FTP resources and increments their data version
+     *
+     * @param {Object} data FTP delete data
+     * @returns {Promise<string[]>} Deleted resource names
+     */
+    async delete(data) {
+        return this.#mutate(() => this.ftpService.delete(data));
+    }
+
+    async #mutate(callback) {
+        return this.tx(async (clientTx) => {
+            const result = await callback();
+            await this.dataVersionsService.increment({
+                clientTx,
+                id: "ftp"
+            });
+            return result;
+        });
+    }
+}
